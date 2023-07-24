b13c0a6a638045517d121dc3c20a1319dd4c1e51
Mehreen Waqar <mehreen.waqar@systemsltd.com>
2023-07-24 22:03:50 +0500

    XCFramework created

---
+++ archives/FBAnal.xcframework/ios-arm64_x86_64-simulator/FBAnal.framework/Headers/FBAnal.h
@@ -0,0 +1,19 @@
+//
+//  FBAnal.h
+//  FBAnal
+//
+//  Created by Mehreen Waqar on 24/07/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FBAnal.
+FOUNDATION_EXPORT double FBAnalVersionNumber;
+
+//! Project version string for FBAnal.
+FOUNDATION_EXPORT const unsigned char FBAnalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FBAnal/PublicHeader.h>
+
+
+#import <FBAnal/FirebaseMethods.h>
